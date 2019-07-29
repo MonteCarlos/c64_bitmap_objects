2a6d618040492a20cb7f4b6d9d7ee3a04c764179
mc78 <mc78@outlook.de>
2019-07-29 07:04:40 +0200

    Added assembly code to generate 24bit value with repeated bits from 8bit value

--- _spritefont.s
+++ _spritefont.s
@@ -1,1 +1,32 @@
 
+    ldx #15
+    axs #0 ;puts lower 4 bits of a into x
+    ldy loBytesTable,x
+    sty lo
+    ldy hiBytesTable,x
+    sty hi
+    lsr
+    lsr
+    lsr
+    lsr
+    tax
+    lda loBytesTable,x
+    asl
+    rol hi+1
+    asl
+    rol hi+1
+    asl
+    rol hi+1
+    asl
+    rol hi+1
+    ora hi
+    sta hi
+    lda hiBytesTable,x
+    asl
+    asl
+    asl
+    asl
+    ora hi+1
+    sta hi+1
+    rts
+
--- spritefont.c
+++ spritefont.c
@@ -1,3 +1,38 @@
-void main(void){
+#include <stdio.h>
+
+unsigned char loBitsTable[] = {
+    0x00, 0x07, 0x38, 0x3f, 0xc0, 0xc7, 0xf8, 0xff,
+    0x00, 0x07, 0x38, 0x3f, 0xc0, 0xc7, 0xf8, 0xff
+};
+
+unsigned char hiBitsTable[] = {
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01,
+    0x0e, 0x0e, 0x0e, 0x0e, 0x0f, 0x0f, 0x0f, 0x0f
+};
+
+void PrintBits (unsigned long int l) {
+    if (l) {
+        PrintBits (l >> 1);
+        putchar (l & 1 ? '1' : '0');
+    }
 }
 
+unsigned long int mkBitTriples (unsigned char num) {
+    unsigned long int result =
+        (unsigned long int) loBitsTable[num & 15] |
+        (unsigned long int)hiBitsTable[num & 15] << 8 |
+        (unsigned long int)loBitsTable[num >> 4] << 12 |
+        (unsigned long int)hiBitsTable[num >> 4] << 20;
+    return result;
+}
+
+int main (void) {
+    unsigned char number = 0xaa;
+    unsigned long int result;
+
+    PrintBits (number);
+    putchar ('\n');
+    result = mkBitTriples (number);
+    PrintBits (result);
+    return 0;
+}
