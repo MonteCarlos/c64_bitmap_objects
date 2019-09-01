393a6111b94111e3358d3656c14800f5970f3b8f
mc78 <mc78@outlook.de>
2019-09-01 07:33:01 +0200

    Added some ignores

---
+++ garbageFiller.s
@@ -0,0 +1,19 @@
+.export fillGarbage = *
+    ldy #$0f
+fill1:
+    ldx #0
+fill2:
+src = *+1
+    lda $e000,x
+dest = *+1
+    sta $2000,x
+    inx
+    bne fill2
+
+    inc src+1
+    inc dest+1
+
+    dey
+    bpl fill1
+    rts
+
