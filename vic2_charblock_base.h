7c9b7bac6c4dc793ebe344e8045eccb1f102f240
mc78 <mc78@outlook.de>
2019-09-27 16:59:19 +0200

    Added check in equality assignment operator when accessing same object. Added equality comparison operator. Added lsr(index, N) method. Reworked VIC2_Charblock lsr2 and lsr1 methods to call lsr base method with appropriate params.

--- vic2_charblock_base.h
+++ vic2_charblock_base.h
@@ -9,8 +9,6 @@
 class VIC2_CharblockBase : public VIC2_StorableBitmapBase {
 protected:
     enum { BYTESPERCHARBLOCK };
-    VIC2_Bitmap_Byte_t Rows[8];
-
 public:
     // Default constructor
     VIC2_CharblockBase() : VIC2_CharblockBase (0) {
@@ -21,32 +19,6 @@ public:
         VIC2_StorableBitmapBase::set(fillvalue);
     }
 
-    /*VIC2_Bitmap_Byte_t get (uint8_t RowNr) {
-        return bitmap[RowNr];
-    }*/
-
-    void set (VIC2_Bitmap_Byte_t (*buf[8]) ) {
-        memcpy ( Rows, buf, sizeof (Rows) );
-    }
-
-    /*void set (VIC2_Bitmap_Byte_t fillvalue) {
-        set ( Rows, fillvalue );
-    }*/
-
-    bool fwrite (std::ofstream &file) {
-        return VIC2_StorableBitmapBase::fwrite(file);
-        //file.write ( (char *) Rows, sizeof (Rows) );
-        //return !file.fail();
-    }
-
-    bool fread (std::ifstream &file) {
-        //file.read ( (char *) Rows, sizeof (Rows) );
-        //return !file.fail();
-        return VIC2_StorableBitmapBase::fread(file);
-    }
-
-    void operator *= (uint8_t factor);
-    bool operator == (VIC2_CharblockBase &block);
 };
 
 
