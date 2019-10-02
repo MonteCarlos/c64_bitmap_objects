b0b4a2b81d8ac265dd49d3a90c61e85848d628b5
mc78 <mc78@outlook.de>
2019-10-02 16:20:37 +0200

    removed lsr1 and lsr2 method from vic2_CHarset class. Not needed anymore.

--- vic2_charblock.h
+++ vic2_charblock.h
@@ -6,14 +6,4 @@ class VIC2_Charblock: public VIC2_CharblockBase {
 public:
     using VIC2_CharblockBase::VIC2_CharblockBase;
 
-    uint8_t lsr2 (int row) {
-        uint8_t bits = shiftRight(row, 2);
-
-        return bits;
-    }
-
-    uint8_t lsr1 (int row) {
-        uint8_t bit = shiftRight(row, 1);
-        return bit;
-    }
 };
