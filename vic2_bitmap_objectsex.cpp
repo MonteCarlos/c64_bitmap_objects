941386d5e5489f4bd48857b5a7ddc85d7bc296f9
mc78 <mc78@outlook.de>
2019-10-08 13:14:55 +0200

    added methods in derived class PixelStream for conversion and output

--- vic2_bitmap_objectsex.cpp
+++ vic2_bitmap_objectsex.cpp
@@ -34,3 +34,34 @@ uint8_t VIC2_BitmapObjectsEx::rotateLeftBy2 (size_t index, uint8_t newBits){
     return rotateRight(index, -2, newBits);
 }
 
+uint8_t VIC2_BitmapObjectsEx::shiftRightBy1 (const std::vector<uint8_t>::iterator &it){
+    return shiftRightBy1(it-begin());
+}
+
+uint8_t VIC2_BitmapObjectsEx::shiftRightBy2 (const std::vector<uint8_t>::iterator &it){
+    return shiftRightBy2(it-begin());
+}
+
+uint8_t VIC2_BitmapObjectsEx::rotateRightBy1 (const std::vector<uint8_t>::iterator &it, uint8_t newBits){
+    return rotateRightBy1(it-begin(), newBits);
+}
+
+uint8_t VIC2_BitmapObjectsEx::rotateRightBy2 (const std::vector<uint8_t>::iterator &it, uint8_t newBits){
+    return rotateRightBy2(it-begin(), newBits);
+}
+
+uint8_t VIC2_BitmapObjectsEx::shiftLeftBy1 (const std::vector<uint8_t>::iterator &it){
+    return shiftLeftBy1(it-begin());
+}
+
+uint8_t VIC2_BitmapObjectsEx::shiftLeftBy2 (const std::vector<uint8_t>::iterator &it){
+    return shiftLeftBy2(it-begin());
+}
+
+uint8_t VIC2_BitmapObjectsEx::rotateLeftBy1 (const std::vector<uint8_t>::iterator &it, uint8_t newBits){
+    return rotateLeftBy1(it-begin(), newBits);
+}
+
+uint8_t VIC2_BitmapObjectsEx::rotateLeftBy2 (const std::vector<uint8_t>::iterator &it, uint8_t newBits){
+    return rotateLeftBy2(it-begin(), newBits);
+}
